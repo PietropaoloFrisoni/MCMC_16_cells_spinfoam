ce4dbefd7e0a9b2524c37a78f8e2bce4d939e923
PietropaoloFrisoni <frisus@outlook.it>
2022-02-19 21:47:40 +0100

    added multithreaded python interface

--- spinfoam.py
+++ spinfoam.py
@@ -1,5 +1,6 @@
-from ctypes import *
+from ctypes import * 
 import os
+import threading
 
 
 def spinfoam_compile():
@@ -20,8 +21,23 @@ def Metropolis_Hastings_run(draws_path, spin, length, sigma, burnin, verbosity, 
                                           b"./data_folder/hashed_21j",
                                           int(2 * spin), length,
                                           c_double(sigma), burnin, verbosity, thread_id)
-
-
+    
+    
+def Metropolis_Hastings_multithread_run(draws_path, spin, length, sigma, burnin, verbosity, number_of_threads):
+    threads = []
+    for thread_id in range(1, number_of_threads+1):
+     t = threading.Thread(target=Metropolis_Hastings_run, args=(draws_path, spin, length, sigma, burnin, verbosity, thread_id,))
+     threads.append(t)
+     print(f'Starting run from thread {thread_id}...')   
+     t.start()
+    
+    for t in threads:   
+     t.join()     
+     
+    print(f'All threads completed the run') 
+    threads.clear()
+    
+    
 def Hashing_21j_symbols(hash_tables_path, spin):
     spinfoam_functions = CDLL("./lib/libspinfoam.so")
     spinfoam_functions._Z19Hashing_21j_symbolsPci(hash_tables_path.encode(), int(2 * spin))                                         
